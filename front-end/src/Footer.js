0f6627860531f15ea7ced1f7a28e28d825a251db
ml6754 <ml6754@nyu.edu>
2022-10-11 13:20:24 -0400

    simple header and footer

---
+++ front-end/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => (
+  <div className = "footer">
+    Mark Lung, Zack Goldberg, Aditya Pradeep,Geoffrey Budiman, Rayhan Ahmed
+    {/*this is probs terrible way to do this*/} 
+    <br/>
+    <br />Woot woot footer pretty cool right
+  </div>
+)
+export default Footer;
---
+++ front-end/src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.css';
+const Header = () => (
+  <div className = "header">
+    ResyBot Logo
+  </div>
+)
+export default Header;
