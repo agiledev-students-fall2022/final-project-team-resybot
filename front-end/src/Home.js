6ba2c56db44a637522f3d3b39f8d9185272ef60e
geoffreybudiman91 <gkb8774@nyu.edu>
2022-10-18 17:06:48 -0400

    Changed style of header and hamburger icon

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,4 +1,4 @@
- /**
+/**
  * A React component that represents the Home page of the app.
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
