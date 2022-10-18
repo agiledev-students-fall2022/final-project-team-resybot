5caa4d833f5a8192bcb3c7d3e1276e877b4f1391
ml6754 <ml6754@nyu.edu>
2022-10-17 22:20:20 -0400

    added images and placed them in the correct place

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Header.css';
+import logo from './resyBotLogo.png'
+import hamburger from './hamburger.png'
 const Header = () => (
   <div className = "header">
-    ResyBot Logo
+    <div className = "header-logo">
+      <img src= {logo} alt = "Logo" />
+    </div>
+    <div className = "header-hamburger">
+      <img src={hamburger} alt= "header-hamburger" />
+    </div>
   </div>
 )
 export default Header;
