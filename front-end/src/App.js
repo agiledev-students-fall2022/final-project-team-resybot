f7e021494957e92a6b8ce0d1f070916a447a1467
geoffreybudiman91 <gkb8774@nyu.edu>
2022-10-18 05:10:19 -0400

    added Sidebar.js and Sidebar.css files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,23 @@
 import Header from './Header';
 import Footer from './Footer'
 import './App.css';
+import SideBar from "./Sidebar";
 
 const App = () => {
   return (
-   <div className = "App">
-    <div>
-      <Header />
+   <div className = "App" id="outer-container">
+      <SideBar pageWrapID={"page-wrap"} outerContainerId={"outer-container"} />
+      <div id="page-wrap">
+        <div>
+          <Header />
+        </div>
+        <div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
     </div>
-    <div>
-      BODY Of EACH PAGE
-    </div>
-    <div>
-      <Footer />
-    </div>
-   </div>
   );
 }
 
