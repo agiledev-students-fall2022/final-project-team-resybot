8a4fc68b9d00c6a20fa6abc3461129a68d9dbbbc
ml6754 <ml6754@nyu.edu>
2022-10-20 12:53:32 -0400

    Login.js Creation

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 const Login = (props) => (
   <div className = "login" id>
-        <Link to ="/" className = "header-logo">
-       </Link>
   </div>
 )
 export default Login;
