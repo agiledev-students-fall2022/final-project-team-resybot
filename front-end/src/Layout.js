f531e3472073b5e6e3a22a32b0d9e2793695ffb6
ml6754 <ml6754@nyu.edu>
2022-10-20 20:19:31 -0400

    fixing stuff

--- front-end/src/Layout.js
+++ front-end/src/Layout.js
@@ -3,21 +3,11 @@ import Footer from './Footer'
 import {useLocation} from 'react-router-dom';
 
 const Layout =({children}) =>{
-    const location = useLocation();
-    if(location.pathname === "/Login")
-    return (
-        <>
-        <main>{children}</main>
-        </>
-    )
-    else
-    return (
-        <>
-        <Header/>
-        <main>{children}</main>
-        <Footer/>
-        </>
-    )
+    <>
+    <Header/>
+    <main>{children}</main>
+    <Footer/>
+    </>
 }
 
 export default Layout;
