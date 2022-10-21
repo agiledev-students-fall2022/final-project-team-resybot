67415deae63ee6d4957038d3a8b174fcec5c1454
ml6754 <ml6754@nyu.edu>
2022-10-20 20:15:47 -0400

    layoutstuff I need rto dix

--- front-end/src/Layout.js
+++ front-end/src/Layout.js
@@ -2,13 +2,11 @@ import Header from './Header'
 import Footer from './Footer'
 
 const Layout =({children}) =>{
-    return (
-        <>
-        <Header/>
-        <main>{children}</main>
-        <Footer/>
-        </>
-    )
+    <>
+            <Header/>
+                <main>{children}</main>
+            <Footer/>
+    </>
 }
 
 export default Layout;
