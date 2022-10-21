10adc0f7064154386700ffe76906ea66de732aeb
ml6754 <ml6754@nyu.edu>
2022-10-20 20:19:31 -0400

    fixing stuff

--- front-end/src/Layout.js
+++ front-end/src/Layout.js
@@ -2,11 +2,13 @@ import Header from './Header'
 import Footer from './Footer'
 
 const Layout =({children}) =>{
-    <>
-    <Header/>
-    <main>{children}</main>
-    <Footer/>
-    </>
+    return (
+        <>
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+        </>
+    )
 }
 
 export default Layout;
