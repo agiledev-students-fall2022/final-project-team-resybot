c30f53ca089be05097561d2a87aee3c3d6b5a91c
ml6754 <ml6754@nyu.edu>
2022-10-21 13:54:02 -0400

    Layout now removes header and footer from Login

--- front-end/src/Layout.js
+++ front-end/src/Layout.js
@@ -1,13 +1,22 @@
 import Header from './Header'
 import Footer from './Footer'
-import {useLocation} from 'react-router-dom';
 
 const Layout =({children}) =>{
-    <>
-    <Header/>
-    <main>{children}</main>
-    <Footer/>
-    </>
+    const location = useLocation();
+    if(location.pathname === "/Login")
+    return (
+        <>
+        <main>{children}</main>
+        </>
+    )
+    else
+    return (
+        <>
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+        </>
+    )
 }
 
 export default Layout;
