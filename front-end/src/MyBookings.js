5476c78f323f7a47480c25d7857605b57a9527ad
zrg228 <zrg228@nyu.edu>
2022-10-26 16:14:02 -0400

    list data from mockaroo

--- front-end/src/MyBookings.js
+++ front-end/src/MyBookings.js
@@ -1,11 +1,48 @@
 import './MyBookings.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Button } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+//import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
 const MyBookings = props => {
+
+  const [Resturaunts , setRes] = useState([])
+  
+  const fetchResy = async () => {
+    const response = await fetch("https://api.mockaroo.com/api/c2496c90?count=10&key=459f4720")
+    const stuff = await response.json()
+    setRes(stuff)
+
+  }
+
+  useEffect(() => {
+    fetchResy()
+  }, [])
+
+
+  return (
+    <div>
+      {Resturaunts.length > 0 && (
+          <ul className= 'lists'>
+            {Resturaunts.map(res => (
+              <li key={res["id"]}>
+                <p className='asd'>
+                  <Link to="/makerequest" className="btn btn-primary">{res["Resturant name"]}</Link>
+                  location {res["location"]}
+                </p>
+                
+              </li>))}
+
+          </ul>
+      )} 
+
+    </div>
+  )
+
+
+/*
+  
   return (
     <div className="MyBookings">
       <h1>Current Bookings</h1>
@@ -16,11 +53,12 @@ const MyBookings = props => {
         <p> <Button variant="contained">Reservation #4</Button> Burger joint</p>
       </main>
       <bottom className='bottom'>
-      <Link to="/Requests" className="btn btn-primary">New Reservation</Link>
+      <Link to="/makerequest" className="btn btn-primary">New Reservation</Link>
       </bottom>
     </div>
      //<button onclick="location.href = 'www.google.com';" id="myButton" class="btn btn-primary btn-lg btn-block">Make a new reservation</button>
   )
+  */
 }
 
 export default MyBookings
