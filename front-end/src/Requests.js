e64c9bc62b8af750eedd00c32fb49d9cb8676669
ml6754 <ml6754@nyu.edu>
2022-10-27 01:16:07 -0400

    makerequests adds requests to the api

--- front-end/src/Requests.js
+++ front-end/src/Requests.js
@@ -3,12 +3,25 @@ import React, { useEffect } from 'react'
 import mockData from './mockdata/requests.json'
 import {Box} from '@mui/material';
 
+const showRequests = ({setCartItems, data}) => {
+  let list = data.map((val) => 
+    <div className="template" key={val.id}>
+        <Box className="description">
+          <div className = "itemControl"> Restaurant: <div className = "valueControl">{val.restaurant} </div></div>
+          <div className = "itemControl"> Party Size: <div className = "valueControl">{val.party_size}</div></div>
+          <div className = "itemControl"> Expiration Date: <div className = "valueControl">{val.expiration_date}</div></div>
+        </Box>
+    </div> 
+  )
+  setCartItems(list)
+};
 
-
-
-
-
-const Requests = ({cartItems,setCartItems,fetchRequests}) => {
+const fetchRequests = async ({setCartItems}) => {
+    const response =  await fetch("https://635740569243cf412f954e2c.mockapi.io/api/rb/Request")
+    const data = await response.json()
+    showRequests({setCartItems,data})
+}
+const Requests = ({cartItems,setCartItems}) => {
     useEffect(()=>{fetchRequests({setCartItems})},[])
     return(
         <div>
