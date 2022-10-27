d8fb1c65baa1db7e1e65a14cbe3f8ea474c310c3
ml6754 <ml6754@nyu.edu>
2022-10-26 22:52:40 -0400

    pushing to the list doesnt add for now

--- front-end/src/MakeRequest.js
+++ front-end/src/MakeRequest.js
@@ -1,11 +1,13 @@
 import './MakeRequest.css';
 import React, { useEffect } from 'react'
 import StandaloneRequest from './StandaloneRequest';
+import mockData from './mockdata/requests.json'
 
 
 const MakeRequest =({cartItems,setCartItems}) => {
     const onAdd = () => {
-        setCartItems([...cartItems,<StandaloneRequest/>])
+    let list =  mockData
+        list.push({"id":"${mockData.length + 1}","restaurant":"test","party_size":"test_size","expiration_date":"test_date"})
     }
     
     return (
