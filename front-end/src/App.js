182583dafb7e54cc01499efdb944b577a8d13c77
ml6754 <ml6754@nyu.edu>
2022-10-26 21:36:41 -0400

    very basic request now takes api info

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MyBookings from'./MyBookings'
 import Layout from "./Layout";
 import Login from "./Login"
@@ -14,19 +14,18 @@ import EditInformation from './EditInformation';
 const App = () => {
   // for adding items to a cart
   const [cartItems, setCartItems] = useState([]);
-  
 
-  
   return (
    <div className = "App">
     <Router> 
           <Layout>
             <Routes>
               <Route path="/" element={<MyBookings />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/Settings" element={<Settings />} />
-              <Route path="/EditInformation" element={<EditInformation />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp/>}/>
+              <Route path="/settings" element={<Settings/>}/>
               <Route path="/requests" element={<Requests cartItems = {cartItems}/>}/>
+              <Route path="/editinformation" element={<EditInformation />} />
               <Route path="/makerequest" element={<MakeRequest cartItems = {cartItems} setCartItems = {setCartItems}/>}/>
             </Routes>
           </Layout>
