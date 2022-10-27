f70036ed7a5c82275d31bfcf6618f6320799be2d
ml6754 <ml6754@nyu.edu>
2022-10-27 10:24:06 -0400

    fixing stuff

--- front-end/src/MakeRequest.js
+++ front-end/src/MakeRequest.js
@@ -1,28 +1,25 @@
 import './MakeRequest.css';
 import React, { useEffect } from 'react'
 import StandaloneRequest from './StandaloneRequest';
-import { useLocation } from "react-router-dom";
 
 
-const MakeRequest = (props) => {
-// const addRequests = async () => {
-//     let item={"id":"${mockData.length + 1}","restaurant":"test","party_size":"test_size","expiration_date":"test_date"}
-//     let result = await fetch("https://635740569243cf412f954e2c.mockapi.io/api/rb/Requests", {
-//         method: 'POST',
-//         headers:{
-//             "Content-Type":"application/json",
-//             "Accept":'application/json'
-//         },
-//         body:JSON.stringify(item)
-//         })
-//     result = await result.json();
-// }
+const MakeRequest =() => {
+const addRequests = async () => {
+    let item={"id":"${mockData.length + 1}","restaurant":"test","party_size":"test_size","expiration_date":"test_date"}
+    let result = await fetch("https://635740569243cf412f954e2c.mockapi.io/api/rb/Requests", {
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":'application/json'
+        },
+        body:JSON.stringify(item)
+        })
+    result = await result.json();
+}
 
     // const onAdd = () => {
     //     let data.push({"id":"${mockData.length + 1}","restaurant":"test","party_size":"test_size","expiration_date":"test_date"})
     // }
-    const location = useLocation();
-    const { restaurant } = location.state;
 
     return (
     <div className = "makeRequest">
