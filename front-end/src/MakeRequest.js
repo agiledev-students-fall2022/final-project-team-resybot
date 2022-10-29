00e5fa94c32b2b20735ccefe87e98e3e86a99a4a
ml6754 <ml6754@nyu.edu>
2022-10-28 20:15:38 -0400

    it works!

--- front-end/src/MakeRequest.js
+++ front-end/src/MakeRequest.js
@@ -1,13 +1,18 @@
 import './MakeRequest.css';
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import { useLocation } from "react-router-dom";
 
 const MakeRequest = () => {
-    
+    useEffect(() => {
+    }, [])
     const [partySize, setPartySize] = useState(0);
     const [time, setTime] = useState("");
     const [expiration_date, setExpirationDate] = useState("");
-    const addRequests = async ({restaurant,partySize,time,expiration_date}) => {
+
+    const addRequests = async ({restaurant},partySize,time,expiration_date) => {
+      console.log(partySize)
+      console.log(time)
+      console.log(expiration_date)
       let item={"restaurant":restaurant.restaurant_name,"party_size":partySize,"time": time,"expiration_date":expiration_date}
       let result = await fetch("https://635740569243cf412f954e2c.mockapi.io/api/rb/Requests", {
           method: 'POST',
@@ -37,7 +42,7 @@ const MakeRequest = () => {
         <input type = "date"  placeholder = "Expiration Date"  id = "expiration-date"
         onChange = {(e)=>setExpirationDate(e.target.value)}
         className = "form-control"/>
-        <button onClick={() => addRequests(restaurant,partySize,time,expiration_date)}> Make Request </button>
+        <button onClick={() => addRequests({restaurant},partySize,time,expiration_date)}> Make Request </button> 
     </div>
   )}
   export default MakeRequest;
