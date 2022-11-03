78b170ca3171cf11d3628820419d43787dba2e9d
ml6754 <ml6754@nyu.edu>
2022-11-03 13:05:18 -0400

    changed server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,14 @@
-const express = require('express');
-
-const path = require('path');
-
-const app = express();
-
-const port = 3000;
-
-app.get('/', (request, response) => {
-    response.send('Yes, this works');
+#!/usr/bin/env node
+const server = require("./app") // load up the web server
+const port = 3000 // the port to listen to for incoming requests
+// call express's listen function to start listening to the port
+const listener = server.listen(port, function () {
+  console.log(`Server running on port: ${port}`)
 })
-
-app.listen(port, () => {
-    console.log('Express server listening on port ${port}');
-})
+// a function to stop listening to the port
+const close = () => {
+  listener.close()
+}
+module.exports = {
+  close: close,
+}
