d1947e08f83a2c7aa8e817f9879d14c97638866a
ml6754 <ml6754@nyu.edu>
2022-11-02 20:48:50 -0400

    fixed css

--- front-end/src/Requests.js
+++ front-end/src/Requests.js
@@ -3,15 +3,24 @@ import React, { useEffect } from 'react'
 import mockData from './mockdata/requests.json'
 import {Box} from '@mui/material';
 import { useState } from 'react';
+import { Button } from 'reactstrap';
 
 const showRequests = ({setCartItems, data}) => {
   let list = data.map((val) => 
-    <div className="template" key={val.id}>
-        <Box className="description">
-          <div className = "itemControl"> Restaurant: <div className = "valueControl">{val.restaurant} </div></div>
-          <div className = "itemControl"> Party Size: <div className = "valueControl">{val.party_size}</div></div>
-          <div className = "itemControl"> Time: <div className = "valueControl">{val.time}</div></div>
-          <div className = "itemControl"> Expiration Date: <div className = "valueControl">{val.expiration_date}</div></div>
+    <div className="requestsTemplate" key={val.id}>
+        <Box className="requestsDescription">
+          <div class="requestsColumnLeft">
+            <div className = "requestsItemControl"> Restaurant: <div className = "requestsValueControl">{val.restaurant} </div></div>
+            <div className = "requestsItemControl"> Party Size: <div className = "requestsValueControl">{val.party_size}</div></div>
+            <div className = "requestsItemControl"> Time: <div className = "requestsValueControl">{val.time}</div></div>
+            <div className = "requestsItemControl"> Expiration Date: <div className = "requestsValueControl">{val.expiration_date}</div></div>      
+          </div>
+          <div class="requestsColumnRight">
+            {/*doesn't do anything yet*/}
+            <Button className="delete">
+                Delete 
+            </Button>
+          </div>
         </Box>
     </div> 
   )
