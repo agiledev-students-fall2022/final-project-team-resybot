f8581257bd08179e0a12dec446f9a40f6bf4a8df
Aditya Pradeep <ap6518@nyu.edu>
2022-11-08 17:06:55 -0500

    fill router

--- back-end/routes/makerequests.js
+++ back-end/routes/makerequests.js
@@ -5,11 +5,11 @@ require("dotenv").config({ silent: true })
 
 //do stuff here
 router.get('/',(req,res) => {
-    //do stuff in here
-        /*axios
-        .get('')
-        .then()
-    */
+    axios
+    .get(`${process.env.API_BASE_URL+process.env.REQUEST}?&key=${process.env.API_KEY}`)
+    .then(apiResponse => {
+        res.json(apiResponse.data)
+    })
 })
 
 module.exports = router;
