8ffb755422d1ce5bc0c7274e4fcb91f49e370de1
adityapradeep12 <75046016+adityapradeep12@users.noreply.github.com>
2022-11-09 16:48:57 -0500

    Removed makerequest + added axios

--- front-end/src/MakeRequest.js
+++ front-end/src/MakeRequest.js
@@ -1,6 +1,7 @@
 import './MakeRequest.css';
 import React, { useState } from 'react'
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const MakeRequest = () => {
     const [partySize, setPartySize] = useState(0);
@@ -9,10 +10,17 @@ const MakeRequest = () => {
     const navigate = useNavigate();
 
     const addRequests = async ({restaurant}) => {
-        let result = await axios.post("/requests", {
-          "restaurant":restaurant.restaurant_name,"party_size":partySize,"time": time,"date":date
-          })
-        result = await result;
+        let item={"restaurant":restaurant.restaurant_name,"party_size":partySize,"time": time,"date":date}
+        let result = await fetch("/requests", {
+            method: 'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":'application/json'
+            },
+            body:JSON.stringify(item)
+            })
+        result = await result.json();
+        console.log(result)
         let path = "/requests"; 
         navigate(path);
       };
