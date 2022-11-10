eb950ee3371dc45a4901a34a0bf2c1727984a4a5
ml6754 <ml6754@nyu.edu>
2022-11-09 21:18:14 -0500

    css changes Make Request now shows restaurant

--- back-end/routes/requests.js
+++ back-end/routes/requests.js
@@ -4,7 +4,6 @@ const axios = require('axios')
 require("dotenv").config({ silent: true })
 
 router.get("/",(req,res) => {
-    //do stuff in here
     axios
         .get(`${process.env.API_BASE_URL+process.env.REQUEST}?&key=${process.env.API_KEY}`)
         .then(apiResponse => {
@@ -13,10 +12,21 @@ router.get("/",(req,res) => {
 })
 
 router.post("/", async (req,res) => {
-    //do stuff in here
     try{
         const resp = await axios
         .post(`${process.env.API_BASE_URL+process.env.REQUEST}?&key=${process.env.API_KEY}`,{ restaurant: req.body.restaurant, party_size: req.body.party_size, time: req.body.time, date: req.body.date})
+        .then(apiResponse =>{
+            res.json(apiResponse.data)
+        })
+    } catch (error) {
+        console.log(error.response)
+    }
+})
+
+router.delete("/:id", async (req,res) => {
+    try{
+        const resp = await axios
+        .delete(`${process.env.API_BASE_URL+process.env.REQUEST}?&key=${process.env.API_KEY}`,{id: req.params.id})
         .then(apiResponse =>{
             res.json(apiResponse.data)
         })
