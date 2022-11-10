217a8882681314f02633830eb23cb04f136f8c1f
ml6754 <ml6754@nyu.edu>
2022-11-10 00:05:15 -0500

    fixes

--- back-end/test/unit-tests-bookings.js
+++ back-end/test/unit-tests-bookings.js
@@ -8,4 +8,15 @@ chai.use(chaiHttp)
 const server = require("../server")
 
 
-chai.use(chaiHttp)
+describe("GET request to /bookings", () => {
+    it("it should respond with 200 status and object",done => {
+        chai
+            .request(server)
+            .get("/bookings")
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a("Array")
+                done()
+            })
+    })
+})
