79362d801481c25bd6d3fd3ea5f49a3ab291e63e
ml6754 <ml6754@nyu.edu>
2022-11-28 16:30:17 -0500

    basic login

--- back-end/validation.js
+++ back-end/validation.js
@@ -12,5 +12,14 @@ const registerValidation = (data) => {
     )
     return schema.validate(data)
 }
+const loginValidation = (data) => {
+    const schema = Joi.object(
+        {
+            email: Joi.string().min(3).max(255).required(),
+            password: Joi.string().min(6).max(255).required(),
+        }
+    )
+    return schema.validate(data)
+}
 
-module.exports = { registerValidation}
+module.exports = {registerValidation, loginValidation}
