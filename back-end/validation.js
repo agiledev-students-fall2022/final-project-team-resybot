16f053c4c27c16afcc180b0882f9f16c293312c4
ml6754 <ml6754@nyu.edu>
2022-11-28 16:30:17 -0500

    basic login

--- back-end/validation.js
+++ back-end/validation.js
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const express = require('express')
 require("dotenv").config({ silent: true })
 const jwt = require('jsonwebtoken')
 const Joi = require('joi');
 
 // basic validation of registration
-=======
-=======
 const express = require('express')
 require("dotenv").config({ silent: true })
 const jwt = require('jsonwebtoken')
->>>>>>> 0794272 (basic verification)
 const Joi = require('joi');
 
-<<<<<<< HEAD
-// basic validating registration
->>>>>>> 210465c (working basic registration)
-=======
 // basic validation of registration
->>>>>>> 0794272 (basic verification)
 const registerValidation = (data) => {
     const schema = Joi.object(
         {
@@ -30,53 +20,5 @@ const registerValidation = (data) => {
     )
     return schema.validate(data)
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-// basic validation of login 
-=======
->>>>>>> 3ce3625 (basic login)
-=======
-// basic validation of login 
->>>>>>> 0794272 (basic verification)
-const loginValidation = (data) => {
-    const schema = Joi.object(
-        {
-            email: Joi.string().min(3).max(255).required(),
-            password: Joi.string().min(6).max(255).required(),
-        }
-    )
-    return schema.validate(data)
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0794272 (basic verification)
-// veryifying our JWT
-const verification = (req,res,next) =>{
-    const token = req.header("auth-token")
-    if(!token){
-        return res.status(401).json({error: "protected data"})
-    }
-    try {
-        const checkVerify = jwt.verify(token, process.env.TOKEN_SECRET)
-        req.user = checkVerify
-        next();
-    }catch(err){
-        res.status(400).json({error: "token not valid"})
-    }
-}
-
-<<<<<<< HEAD
-module.exports = {registerValidation, loginValidation, verification} 
-=======
 
 module.exports = { registerValidation}
->>>>>>> 210465c (working basic registration)
-=======
-
-module.exports = {registerValidation, loginValidation}
->>>>>>> 3ce3625 (basic login)
-=======
-module.exports = {registerValidation, loginValidation, verification} 
->>>>>>> 0794272 (basic verification)
