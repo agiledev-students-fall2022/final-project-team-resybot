80560336800f2d860686c75452a6e34a02f944c0
ml6754 <ml6754@nyu.edu>
2022-11-28 20:34:07 -0500

    fixing merge

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,55 +1,48 @@
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const router = require('express').Router();
-const User = require('../models/user');
-const { registerValidation, loginValidation } = require('../validation');
-
-router.post("/register", async (req, res) => {
+const express = require('express')
+require("dotenv").config({ silent: true })
+const jwt = require('jsonwebtoken')
+const {hash, compare} = require('bcryptjs')
+const {registerValidation, loginValidation} = require('../validation')
 
 const userSchema = require('../models/user')
 const { request } = require('chai')
 const { application } = require('express')
-const { createSearchParams } = require('react-router-dom')
 const user = require('../models/user')
 const router = express.Router()
 require("dotenv").config({ silent: true })
 
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
+router.post('/register', async (req,res) => {
+    //validate user input
+    const {err} = registerValidation(req.body)
+    if(err) {
+        return res.status(400).json({error: error.details[0].message})
     }
 
-    //check if already registered
-    const emailExists = await User.findOne({ email: req.body.email });
-
-    if (emailExists) {
-        return res.status(400).json({ error: "Email already exists" });
+    //check if the email is already registered
+    const emailExist = await userSchema.findOne({email: req.body.email})
+    if(emailExist){
+        return res.status(400).json({error: "email already registered"})
     }
+    //hash the password
+    const hashpswd = await hash(req.body.password, 10)
 
-    //hash password
-    const generatedsalt = await bcrypt.genSalt(10);
-    const hashpswd = await bcrypt.hash(req.body.password, generatedsalt);
-
-    //create user 
-    const user = new User({
+    //create a user
+    const userObject = new userSchema({
         name: req.body.name,
         email: req.body.email,
-        hashpswd
-    });
-
+        password: hashpswd
+    })
     try {
-        const savedUser = await user.save(); 
-        res.json({ error: null, data: savedUser._id });
-    } catch (error) {
-        res.status(400).json({ error });
+        const savedUser = await userObject.save()
+        res.json({error : null, data: savedUser._id});
+    } catch (err) {
+        res.status(400).json({err})
     }
-
 })
-
 
 router.post("/login", async (req,res) => {
    //validate user input
-   const {err} = registerValidation(req.body)
+   const {err} = loginValidation(req.body)
    if(err) {
        return res.status(400).json({error: error.details[0].message})
    }
@@ -66,19 +59,18 @@ router.post("/login", async (req,res) => {
         return res.status(400).json({error: "incorrect password"})
    }
 
-    //create authentication token with username and id
-    const token = jwt.sign(
-        {
-            name: user.name,
-            id: user._id
-        },
-        process.env.TOKEN_SECRET
-    );
-
-    res.header("auth-token", token).json({
+   //create JWT with username and id
+   const token = jwt.sign(
+    {  
+        name: user.name,
+        id: user._id
+    },
+    process.env.TOKEN_SECRET
+   )
+   //attach to header
+   res.header("auth-token", token).json({
         error: null,
-        data: { token }
-    })
-})
+        data: {token}
+   })
 
-module.exports = router
+})
