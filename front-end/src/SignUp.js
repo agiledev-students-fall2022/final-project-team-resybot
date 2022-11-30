a6899d5bbae83967222acfeaa4323f36652313d4
ml6754 <ml6754@nyu.edu>
2022-11-30 14:54:19 -0500

    signUp css

--- front-end/src/SignUp.js
+++ front-end/src/SignUp.js
@@ -5,6 +5,10 @@ const SignUp = () => (
       <br/>
       <h1 className = "signUp-header"> Sign Up </h1>
       <br/>
+          <input type = "text"  placeholder = "Name"  id = "name"
+          //onChange = {(e)=>setEmail(e.target.value)}
+          className = "form-control"/>
+          <br/>
           <input type = "text"  placeholder = "Email"  id = "email"
           //onChange = {(e)=>setEmail(e.target.value)}
           className = "form-control"/>
@@ -14,7 +18,7 @@ const SignUp = () => (
           //onChange = {(e)=>setPassword(e.target.value)}
           className = "form-control"/>
           <br/>
-          <button /*onClick={SignUp}*/ className = "btn" type = "submit">Login</button>
+          <button /*onClick={SignUp}*/ className = "btn" type = "submit">Sign Up</button>
   </div>
 );
 export default SignUp;
