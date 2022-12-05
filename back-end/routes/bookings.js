d61b92e30872f1db609535f2e61168180f900de1
zrg228 <zrg228@nyu.edu>
2022-12-05 16:35:55 -0500

    fixing delete and added date + css

--- back-end/routes/bookings.js
+++ back-end/routes/bookings.js
@@ -34,11 +34,18 @@ router.post("/", async (req,res) => {
         .catch(err => {}) 
 })
 router.delete("id", async (req,res) => {
+<<<<<<< HEAD
     axios.post(`https://staging-api.resy.com/3/cancel`, {
         headers: {
         "authorization": "ResyAPI api_key=\"VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5\"",
         "x-resy-auth-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2NzM4MjkxODYsInVpZCI6MzczMzM4NzcsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMjU0MTM5MDB9fQ.AZeARHcdxaJiIqHu8sraUDPAdYLGeicUmkob5rDDFglr5opobrpxjpx9JdBthNUMBPvIGyoDdh5lxh5-xB60_v-OAIC4vtUoVAF96U4C9omspI1evt2rjlyDiPu-unyVvODFD6IN79BJDDjAOAQk9F3xIUR0Ycd9gPDyW4EIkXnRbAEs"
         
+=======
+    axios.delete(`https://api.resy.com/3/user/reservations/${res.reservation_id}`, {
+        headers: {
+        "authorization": "ResyAPI api_key=\"VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5\"",
+        "x-resy-auth-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2NzM4MjkxODYsInVpZCI6MzczMzM4NzcsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMjU0MTM5MDB9fQ.AZeARHcdxaJiIqHu8sraUDPAdYLGeicUmkob5rDDFglr5opobrpxjpx9JdBthNUMBPvIGyoDdh5lxh5-xB60_v-OAIC4vtUoVAF96U4C9omspI1evt2rjlyDiPu-unyVvODFD6IN79BJDDjAOAQk9F3xIUR0Ycd9gPDyW4EIkXnRbAEs"
+>>>>>>> f5a2e52 (fixing delete and added date + css)
       }
     })
     .then(apiResponse =>{
