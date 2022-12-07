c5151d7c9d744a4d1b6c38289c40ef3892eafcf3
ml6754 <ml6754@nyu.edu>
2022-12-07 00:24:03 -0500

    some changes to time

--- back-end/bot.js
+++ back-end/bot.js
@@ -67,6 +67,7 @@ const runBot = async ({bookingDate, bookingTime, party_size, venueId, timeToRequ
                 //slot time
                 const slotTime = (((JSON.parse(JSON.stringify(slots.at(i))).date.start).slice(-8)).slice(0,5))
                 console.log(slotTime)
+                console.log(bookingTime)
                 //search everyslot for something close enough
                 if(slotTime !== bookingTime){
                     if(slotTime.slice(0,2) === bookingTime.slice(0,2) && (parseInt(slotTime.slice(-2)) + 15 >= parseInt(bookingTime.slice(-2)) && parseInt(slotTime.slice(-2)) - 15 <= parseInt(bookingTime.slice(-2)))){
@@ -111,7 +112,7 @@ const runBot = async ({bookingDate, bookingTime, party_size, venueId, timeToRequ
 }
 
 const helperBookingBot =  async({bookingDate, bookingTime, party_size, xresyauthtoken, resyAPIkey, result, slots}) => { 
-    if(slots.length == 0){
+    if(slots.length === 0){
         console.log("exiting")
         return false
     }
@@ -126,6 +127,7 @@ const helperBookingBot =  async({bookingDate, bookingTime, party_size, xresyauth
                 const slotTime = (((JSON.parse(JSON.stringify(slots.at(i))).date.start).slice(-8)).slice(0,5))
                 console.log("testing")
                 console.log(slotTime)
+                console.log(bookingTime)
                 //search everyslot for something close enough
                 if(slotTime !== bookingTime){
                     if(slotTime.slice(0,2) === bookingTime.slice(0,2) && (parseInt(slotTime.slice(-2)) + 15 >= parseInt(bookingTime.slice(-2)) && parseInt(slotTime.slice(-2)) - 15 <= parseInt(bookingTime.slice(-2)))){
@@ -169,11 +171,11 @@ const helperBookingBot =  async({bookingDate, bookingTime, party_size, xresyauth
 
 //testing for now
 const bookingDate = new Date()
-bookingDate.setDate(18)
-const bookingTime = "5:30"
+bookingDate.setDate(20)
+const bookingTime = "21:30"
 const party_size = 2
 const venueId = 62659
-const timeToRequest = "00:00"
+const timeToRequest = "00:22"
 const xresyauthtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2NzI4ODEwNjksInVpZCI6MzY2MzUzMTMsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMjM5OTE4MDR9fQ.AfJq4ME6M1M3HA0nnQpPmFcNp17mycGffXjrhv6imhk5SDooFaESSB8k-HVo1HD97CghYbCsU-yKorj6YtrRC0C6AHHX44T3Z5gE50l6eZcKapbKkcCJyORErfbuNTT-qT_9mFn1AD0vBlCXiRincANK8HitwRKBppXxWG2hz5YitLls"
 const resyAPIkey = "ResyAPI api_key=\"VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5\""
 
