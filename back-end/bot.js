f1ba86049f5e8b381023018e1f91b1dd2647e850
ml6754 <ml6754@nyu.edu>
2022-12-06 23:35:28 -0500

    update bot

--- back-end/bot.js
+++ back-end/bot.js
@@ -170,9 +170,9 @@ const helperBookingBot =  async({bookingDate, bookingTime, party_size, xresyauth
 //testing for now
 const bookingDate = new Date()
 bookingDate.setDate(18)
-const bookingTime = "12:30"
+const bookingTime = "5:30"
 const party_size = 2
-const venueId = 40734
+const venueId = 62659
 const timeToRequest = "00:00"
 const xresyauthtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2NzI4ODEwNjksInVpZCI6MzY2MzUzMTMsImd0IjoiY29uc3VtZXIiLCJncyI6W10sImV4dHJhIjp7Imd1ZXN0X2lkIjoxMjM5OTE4MDR9fQ.AfJq4ME6M1M3HA0nnQpPmFcNp17mycGffXjrhv6imhk5SDooFaESSB8k-HVo1HD97CghYbCsU-yKorj6YtrRC0C6AHHX44T3Z5gE50l6eZcKapbKkcCJyORErfbuNTT-qT_9mFn1AD0vBlCXiRincANK8HitwRKBppXxWG2hz5YitLls"
 const resyAPIkey = "ResyAPI api_key=\"VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5\""
