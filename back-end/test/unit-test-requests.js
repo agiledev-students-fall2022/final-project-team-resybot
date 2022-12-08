ac04e028139d5649a367c61df0fa529c27dc2541
Aditya Pradeep <ap6518@nyu.edu>
2022-12-07 19:24:13 -0500

    fixed tests

--- back-end/test/unit-test-requests.js
+++ back-end/test/unit-test-requests.js
@@ -5,11 +5,15 @@ const should = chai.should();
 chai.use(chaiHttp)
 const server = require("../server")
 
+const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImlkIjoiNjM4OTUxMWMwYzBjMWU0ZDM2NzE2MjY2IiwiaWF0IjoxNjY5OTQzNTk2fQ.bYq_Oht3NTadhgpEx-Mlx-N-IkTFI3mxA5ENnB1w9e0"
+const owner = "6389511c0c0c1e4d36716266"
+
 describe("GET request to /requests", () => {
     it("it should respond with 200 status and object",done => {
-        chai
-            .request(server)
+        chai.request(server)
             .get("/requests")
+            .set("auth-token", authToken)
+            .set("owner", owner)
             .end((err, res) => {
                 res.should.have.status(200)
                 res.body.should.be.a("Array")
@@ -18,11 +22,14 @@ describe("GET request to /requests", () => {
     })
 })
 
+
 describe("POST request to /requests", () => {
     it("it should respond with 200 status",done => {
         chai
             .request(server)
             .post("/requests")
+            .set("auth-token", authToken)
+            .set("owner", owner)
             .end((err, res) => {
                 res.should.have.status(200)
                 done()
