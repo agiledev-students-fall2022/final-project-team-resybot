b6c5606489b45233b6e037f5081eb726b558cc84
Aditya Pradeep <ap6518@nyu.edu>
2022-12-07 22:28:09 -0500

    test

--- back-end/test/unit-test-requests.js
+++ back-end/test/unit-test-requests.js
@@ -8,31 +8,31 @@ const server = require("../server")
 const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImlkIjoiNjM4OTUxMWMwYzBjMWU0ZDM2NzE2MjY2IiwiaWF0IjoxNjY5OTQzNTk2fQ.bYq_Oht3NTadhgpEx-Mlx-N-IkTFI3mxA5ENnB1w9e0"
 const owner = "6389511c0c0c1e4d36716266"
 
-describe("GET request to /requests", () => {
-    it("it should respond with 200 status and object.",done => {
-        chai.request(server)
-            .get("/requests")
-            .set("auth-token", authToken)
-            .set("owner", owner)
-            .end((err, res) => {
-                res.should.have.status(200)
-                res.body.should.be.a("Array")
-                done()
-            })
-    })
-})
+// describe("GET request to /requests", () => {
+//     it("it should respond with 200 status and object.",done => {
+//         chai.request(server)
+//             .get("/requests")
+//             .set("auth-token", authToken)
+//             .set("owner", owner)
+//             .end((err, res) => {
+//                 res.should.have.status(200)
+//                 res.body.should.be.a("Array")
+//                 done()
+//             })
+//     })
+// })
 
 
-describe("POST request to /requests", () => {
-    it("it should respond with 200 status",done => {
-        chai
-            .request(server)
-            .post("/requests")
-            .set("auth-token", authToken)
-            .set("owner", owner)
-            .end((err, res) => {
-                res.should.have.status(200)
-                done()
-            })
-    })
-})
+// describe("POST request to /requests", () => {
+//     it("it should respond with 200 status",done => {
+//         chai
+//             .request(server)
+//             .post("/requests")
+//             .set("auth-token", authToken)
+//             .set("owner", owner)
+//             .end((err, res) => {
+//                 res.should.have.status(200)
+//                 done()
+//             })
+//     })
+// })
