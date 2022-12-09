5c83347807de6774294aec4deaaf34e132c2b595
ml6754 <ml6754@nyu.edu>
2022-12-08 21:02:06 -0500

    wtf is going on rn

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -13,6 +13,7 @@ const Login = () => {
   }, [])
   async function login(){
     console.warn(email, password)
+    console.log(`${process.env.REACT_APP_BACKEND}/user/login`)
     let result = await axios.post(`${process.env.REACT_APP_BACKEND}/user/login`, {
       "email": email, "password": password
     })
